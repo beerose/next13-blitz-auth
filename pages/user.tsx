75e4f15005ea0ca89d60ec7bb69cb00929631eab
beerose <alexsandra.sikora@gmail.com>
2022-11-04 11:14:29 +0800

    Add dependecies and a simple user page

---
+++ pages/user.tsx
@@ -0,0 +1,47 @@
+import { getAntiCSRFToken } from "@blitzjs/auth";
+import { BlitzPage } from "@blitzjs/next";
+import { useRouter } from "next/router";
+import { SyntheticEvent } from "react";
+import { useSession } from "../src/blitz-client"
+import styles from '../app/page.module.css'
+
+const UserPage: BlitzPage = () => {
+  const router = useRouter()
+  const session = useSession()
+
+  const onLogout = async (e: SyntheticEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    
+    const antiCSRFToken = getAntiCSRFToken()
+    const response = await fetch("/api/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "anti-csrf": antiCSRFToken
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    router.push("/")
+  };
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <h2>User Page</h2>
+        {session.userId ? (
+          <>
+            <p>User id: {session.userId}</p>
+            <p>User email: {session.email}</p>
+            <button onClick={onLogout}>Logout</button>
+          </>
+        ) : "Not authenticated"}
+      </div>
+    </main>
+  )
+}
+
+export default UserPage
